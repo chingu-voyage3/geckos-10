feb9e21d5cfb49154dd0b08256cb1c8c4cb6f974
Adam Martin <adam.madram@gmail.com>
2017-12-31 01:53:14 -0600

    Fix propTypes on develop branch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 class Header extends Component {
   render() {
     let name = this.props.name;
     return (
-      <header className='appHeader'>
+      <header className="appHeader">
         {this.props.authenticated
-          ?
-          <div>
-            <Link to="/logout" className='pt-button pt-minimal pt-icon-log-out' aria-label="Logout"></Link>
+          ? <div>
+            <Link
+              to="/logout"
+              className="pt-button pt-minimal pt-icon-log-out"
+              aria-label="Logout"
+            />
             <p>Logged in as {name}</p>
           </div>
-          : <Link to="/login" className='pt-button pt-intent-primary'>Register or Log In</Link>
-        }
+          : <Link to="/login" className="pt-button pt-intent-primary">
+            Register or Log In
+            </Link>}
       </header>
-    )
+    );
   }
 }
 
+Header.propTypes = {
+  authenticated: PropTypes.bool,
+  name: PropTypes.string
+};
 
 export default Header;
