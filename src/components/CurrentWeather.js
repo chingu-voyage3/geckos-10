9119cbee8e1fc7bf3b30781ff9c0844ff3050f67
Demir Delic <demir.delic@gmail.com>
2018-01-24 05:17:34 -0700

    Finish ultra-basic styling for CurrentWeather

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -6,12 +6,16 @@ class CurrentWeather extends Component {
       <div className="currentWeatherDiv">
         <div className="iconTemp">
           <img className="icon" src={"http://openweathermap.org/img/w/" + this.props.currentWeatherIcon + ".png"} />
-          <h2 className="currentTemp">{this.props.currentTemp}°F</h2>
+          <h1 className="currentTemp">{this.props.currentTemp}°F</h1>
         </div>
-        {/*<p className="description">{this.props.currentWeatherDescription}</p>
-        <p>Low: {this.props.currentMinTemp}°F</p>
-        <p>High: {this.props.currentMaxTemp}°F</p>
-    <p className="city">{this.props.cityName}</p>*/}
+        <div className="descriptionMinMax">
+          <h3 className="description">{this.props.currentWeatherDescription}</h3>
+          <div className="minMax">
+            <h3>Low: {this.props.currentMinTemp}°F</h3>
+            <h3>High: {this.props.currentMaxTemp}°F</h3>
+          </div>
+        </div>
+        <h2 className="city">{this.props.cityName}</h2>
       </div>
     );
   }
